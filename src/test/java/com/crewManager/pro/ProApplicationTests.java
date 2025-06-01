048046d298ad2bac28b7e9e81cea85e985990e39
kkmjj <alswns1201@gmail.com>
2025-06-01 13:51:51 +0900

    초기 프로젝트 시작

---
+++ src/test/java/com/crewManager/pro/ProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crewManager.pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
